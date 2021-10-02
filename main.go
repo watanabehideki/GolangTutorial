d4d658ca8a3ddbe5595b6391f3a3c4d79ac8e1ad
watanabehideki <hideki.art.2280@gmail.com>
2021-10-02 22:49:51 +0900

    constant

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "fmt"
+
+const Pi = 3.14 //頭文字が大文字だと他のパッケージからも飛び出しが可能
+
+const (
+	URL      = "http://example.com"
+	SiteName = "test"
+)
+
+const (
+	A = 1
+	B
+	C
+	D = "あ"
+	E
+	F
+)
+
+const (
+	c0 = iota //連番を返す
+	c1
+	c2
 )
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	fmt.Println(Pi)            // => 3.14
+	fmt.Println(URL, SiteName) // => http://example.com test
+
+	fmt.Println(A, B, C, D, E, F) // => 1 1 1 あ あ あ
+
+	fmt.Println(c1)         // => 1
+	fmt.Println(c0, c1, c2) // => 0 1 2
+
 }
