8ee50f522d71765a1c108475d9e0510b26d585b2
watanabehideki <hideki.art.2280@gmail.com>
2021-10-02 18:38:58 +0900

    variable

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func outer() {
+	outer := "outer関数です!"
+	fmt.Println(outer)
+}
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	// # fmtの使い方
+	// fmt.Println("Hello World")
+	// fmt.Println(time.Now())
+
+	// # 変数と型
+	var i int = 100
+	fmt.Println(i)
+	i = 150
+	fmt.Println(i) // => 150
+
+	var s string = "Hello Go"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int    = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3, s3) // => 0 （初期値として 0, 空文字が入る為)
+
+	// # 暗黙的な定義
+	i4 := 400 // var i4 int = 400 と同じ
+	fmt.Println(i4)
+
+	// i4 = "Hello"
+	// fmt.Println(i4) // => エラー: cannot use "Hello" (type untyped string) as type int in assignment
+
+	outer() // => "outer関数です!"
+
+	// s5 := "Not Use" //エラー: s5 declared but not used
 }
