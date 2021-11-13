dc9776175ce81eb32712a9146c3aabe8d9e7fa3a
watanabehideki <hideki.art.2280@gmail.com>
2021-11-13 17:35:35 +0900

    feature/struct_2

--- main.go
+++ main.go
@@ -2,10 +2,51 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+type T struct {
+	User
+}
+
+type User struct {
+	Name string
+	Age  int
+}
+
+// structメソッド
+func (u User) SayName() {
+	fmt.Println(u.Name)
+}
+
+func (u *User) SetName(name string) { //構造体に定義したフィールドの値を変更する際はレシーバーをポインタ型にする必要がある
+	u.Name = name
+}
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	user1 := User{Name: "user1"}
+	user1.SayName()
+	// => user1
+
+	//Nameを更新する
+	user1.SetName("A")
+	user1.SayName()
+	// => A
+
+	// 構造体の埋め込み
+	t := T{User: User{Name: "userT", Age: 10}}
+	fmt.Println(t)
+	// => {{userT 10}}
+
+	fmt.Println(t.User)
+	// => {userT 10}
+
+	fmt.Println(t.User.Name)
+	// => userT
+
+	fmt.Println(t.Name) // t.User.Name
+	// => userT (type T でUser型を省略記述した場合のみ、t.User.Nameと同じ結果が得られる)
+
+	t.User.SetName("t")
+	fmt.Println(t.User)
+	// => {t 10}
 }
