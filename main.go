8b3bf2d377c97bd33c5efb158f505ef2a794733d
watanabehideki <hideki.art.2280@gmail.com>
2021-11-13 16:22:10 +0900

    feature/pointers

--- main.go
+++ main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+func Double(i int) {
+	i = i * 2
+}
+
+func Double2(i *int) { // 参照渡し
+	*i = *i * 2
+}
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	var n int = 100
+	fmt.Println(n)
+	fmt.Println(&n) // メモリ上のアドレス
+	// => 0xc0000b2008
+
+	Double(n)
+	fmt.Println(n)
+	// => 100
+
+	var p *int = &n
+	// *int でポインタ型をm指定
+	// &n でアドレスを渡す
+
+	fmt.Println(p)
+	// => 0xc0000b2008
+	fmt.Println(*p)
+	// => 100
+
+	// *p = 300
+	// // *p と n は同じアドレスなので上書きされる
+	// fmt.Println(n)
+	// // => 300
+
+	// n = 200
+	// fmt.Println(*p)
+	// // => 200
+
+	Double2(&n)
+	fmt.Println(n)
+	// => 200
+
 }
